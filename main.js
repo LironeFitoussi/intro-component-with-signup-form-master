941f46306f8e1f01843b5d72624f662949861b33
LironeFitoussi <lironefit@icloud.com>
2023-10-12 11:31:31 +0300

    error icon progression

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
-console.log("Test");
+const fNameError = $("<span>").addClass("errMsg");
+fNameError.text("First Name cannot be empty");
+
+const lNameError = $("<span>").addClass("errMsg");
+lNameError.text("Last Name cannot be empty");
+
+const mailError = $("<span>").addClass("errMsg");
+mailError.text("Email cannot be empty");
+
+const pswdError = $("<span>").addClass("errMsg");
+pswdError.text("Password cannot be empty");
+
+const errorIcon = $("<img>").addClass("errIcon").attr("src", "./images/icon-error.svg");
+
+$("form").submit(function (e) {
+  e.preventDefault();
+  $(".errMsg").remove();
+  $(".errIcon").remove();
+  let inputCollection = document.querySelectorAll("input");
+
+  $(inputCollection).each((indexInArray, valueOfElement) => {
+    const inputValue = $(valueOfElement).val();
+    const inputId = $(valueOfElement).attr("id");
+    $(valueOfElement).addClass("wrongInput");
+
+    const clonedErrorIcon = errorIcon.clone(); // Create a clone of the original errorIcon
+
+    if (inputValue === "") {
+      switch (inputId) {
+        case "fNameInput":
+          fNameError.insertAfter(valueOfElement);
+          $("#fNameContainer").append(clonedErrorIcon);
+          break;
+        case "lNameInput":
+          lNameError.insertAfter(valueOfElement);
+          $("#lNameContainer").append(clonedErrorIcon);
+          break;
+        case "mailInput":
+          mailError.insertAfter(valueOfElement);
+          $("#malContainer").append(clonedErrorIcon);
+          break;
+        case "pswdInput":
+          pswdError.insertAfter(valueOfElement);
+          $("#passwordContainer").append(clonedErrorIcon);
+          break;
+      }
+    } else {
+      $(valueOfElement).removeClass("wrongInput");
+    }
+  });
+});
